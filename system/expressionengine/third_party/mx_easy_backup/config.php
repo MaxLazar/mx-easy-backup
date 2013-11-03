31dabec679df5f2643f4dc85ec7178e86413f5fe
Max Lazar <max@eec.ms>
2013-11-03 11:47:25 +0400

    no message

---
+++ system/expressionengine/third_party/mx_easy_backup/config.php
@@ -0,0 +1,18 @@
+<?php
+if (! defined('MX_EASY_BACKUP_KEY'))
+{
+	define('MX_EASY_BACKUP_NAME', 'MX Easy Backup');
+	define('MX_EASY_BACKUP_VER',  '1.1.0');
+	define('MX_EASY_BACKUP_KEY', 'mx_easy_backup');
+	define('MX_EASY_BACKUP_AUTHOR',  'Max Lazar');
+	define('MX_EASY_BACKUP_DOCS',  'http://www.eec.ms/add-ons/mx-easd');
+	define('MX_EASY_BACKUP_DESC',  '');
+
+}
+
+/*
+ * NSM Addon Updater
+$config['name'] = MX_GLOBAL_ENTRY_NAME;
+$config['version'] = MX_GLOBAL_ENTRY_VER;
+$config['nsm_addon_updater']['versions_xml'] = 'http://www.eec.ms/versions/watchdog';
+*/
